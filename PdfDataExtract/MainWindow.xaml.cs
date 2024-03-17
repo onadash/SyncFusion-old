21dfb35cdce171d1fa4b78f9cf1b0deb6e39acb5
Olga <starusevaos@gmail.com>
2024-03-17 11:48:22 +0100

    Initial commit

---
+++ PdfDataExtract/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Text.RegularExpressions;
+
+namespace PdfDataExtract
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var pdfPath = @"Faktura2.pdf";
+            var csvPath = @"Dane do wyszukania.csv";
+
+            TextExtract textExtract = new TextExtract();
+            string pdfText = textExtract.PdfExtract(pdfPath);
+            var searchData = textExtract.CsvExtract(csvPath);
+
+            foreach ( var dataItem in searchData )
+            {
+                string searchPattern = dataItem.Item1 + "[^a-zA-Z0-9]*" + dataItem.Item2;
+                if (Regex.IsMatch(pdfText, @searchPattern))
+                    return;
+            }
+        }
+    }
+}
---
+++ PdfDataExtract/TextExtract.cs
@@ -0,0 +1,60 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+using System.IO;
+
+namespace PdfDataExtract
+{
+    internal class TextExtract
+    {
+        public TextExtract() { }
+
+        public string PdfExtract(string filePath) {
+            
+            FileStream docStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            //Load the PDF document.
+            PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+            // Loading page collections
+            PdfLoadedPageCollection loadedPages = loadedDocument.Pages;
+
+            string extractedText = string.Empty;
+            // Extract text from existing PDF document pages
+            foreach (PdfLoadedPage loadedPage in loadedPages)
+            {
+                extractedText += loadedPage.ExtractText();
+            }
+            //Close the document.
+            loadedDocument.Close(true);
+            return extractedText;
+
+        }
+
+        public IEnumerable<(string?, string)> CsvExtract(string csvPath) {
+
+            var separator = ";";
+
+            var lines = File.ReadLines(csvPath);
+
+            var indexedKeys = lines
+                .First()
+                .Split(separator, StringSplitOptions.None)
+                .ToList();
+
+            var values = lines
+                .Skip(1)
+                .SelectMany(l=>l.Split(separator, StringSplitOptions.None).Select((k, i) =>
+            (
+                Index: i,
+                Value: k
+            ))
+            );
+
+            var result = values
+               .Select((ik) =>
+            (
+                indexedKeys[ik.Index],
+                ik.Value
+            ));
+            return result;
+        }
+    }
+}
